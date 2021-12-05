45c7e8588a67fd724c2cac7ecb69b4979b7c2765
misuzz <fs9dm3cc8ct1@gmail.com>
2021-12-05 18:27:07 +0900

    carousel

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 'user strict';
 
 window.onload = () => setInterval(timeCount, 1000);
-// window.onload = () => showProcess(now);
-
-// var now = new Date();
-// var nowYear = now.getFullYear();
-// var nowMonth = now.getMonth() + 1;
-// var nowDay = now.getDate();
-// var array = new Array("日", "月", "火", "水", "木", "金", "土");
-// var nowYoubi = array[now.getDay()];
-// var nowHour = now.getHours();
-// var nowMinute = now.getMinutes();
-// var nowSecond = now.getSeconds();
-// var showDate = new Date(nowYear, nowMonth, 1); //毎月１日で取得
 
 function timeCount() {
 	let now = new Date();
@@ -33,63 +21,44 @@ function timeCount() {
 	let msg = "<p>" + nowYear + "年" + nowMonth + "月" + nowDay + "日" + "(" + nowYoubi + ")" + nowHour + "時" + nowMinute + "分" + nowSecond + "秒 現在、</p>";
 	document.getElementById("count").innerHTML = msg;
 }
+////////////////////carousel/////////////////////////////////
+
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+const ul = document.getElementById("medakaBabyUl");
+const slides = ul.children; //ulタグの子要素の情報を取る
+const slidewidth = slides[0].getBoundingClientRect().width;
+//getBoundingClientRect()でslides[0]の情報を取れる
+let currentSlide = 0;
+
+//画像の数より多い・少ないとボタンを消す
+function btnHidden() {
+	if (currentSlide == 0) {
+		prev.classList.add('btnHidden')
+	} else if (currentSlide == slides.length - 1) {
+		next.classList.add('btnHidden')
+	} else {
+		prev.classList.remove('btnHidden');
+		next.classList.remove('btnHidden');
+	}
+}
+
+//btnHiddenを実行しつつスライドさせる
+function slideImg() {
+	btnHidden();
+	ul.style.transform = `translateX(${-1 * slidewidth * currentSlide}px)`;
+}
 
 
-//////////////////////////////////////////////////////////////////////
-// function prev() {
-// 	showDate.setMonth(nowMonth - 1);
-// 	showProcess(showDate);
-// }
 
-// function next() {
-// 	showDate.setMonth(nowMonth + 1);
-// 	showProcess(showDate);
-// }
+btnHidden();
 
-// //カレンダータイトルの取得
-// function showProcess(date) {
-// 	var year = date.getFullYear();
-// 	var month = date.getMonth();
-// 	document.getElementById("calenderTitle").innerHTML = year + "年" + (month+1) + "月";
-// 	var calenderContent = createProcess(year, month);
-// 	document.getElementById("calenderContent").innerHTML = calenderContent
-// }
+next.addEventListener('click', () => {
+	currentSlide++;
+	slideImg();
+});
 
-// //カレンダーの中身の作成
-// function createProcess(aYear, aMonth) {
-// 	//tableタグの作成
-// 	var createTableTag = "<table><tr class = 'dayOfWeek'>";
-// 	var p = 0
-// 	while(p < array.length){
-// 		createTableTag += "<th>" + array[p] + "</th>";
-// 		p++;
-// 	}
-// 	createTableTag += "</tr>";
-
-// 	var count = 0;
-// 	var startDayOfWeek = new Date(aYear, aMonth, 1).getDay(); //表示する月の1日の曜日
-// 	var endDate = new Date(aYear, aMonth + 1, 0).getDate(); //表示する月の末日
-// 	var lastMonthEndDate = new Date(aYear, aMonth, 0).getDate();
-// 	var row = Math.ceil((startDayOfWeek + endDate) / array.length); //週の数
-
-// 	for (var i = 0; i < row; i++) {
-// 		createTableTag += "<tr>";
-// 		for (var j = 0; j < array.length; j++) {
-// 			if (i == 0 && j < startDayOfWeek) {
-// 				createTableTag += "<td class='disabled'>" + (lastMonthEndDate - startDayOfWeek + j + 1) + "</td>";
-// 			}else if (count >= endDate){
-// 				count++;
-// 				createTableTag += "<td class='disabled'>" + (count - endDate) + "</td>";
-// 			}else{
-// 				count++;
-// 				if(aYear == nowYear && aMonth == nowMonth && count == nowDay){
-// 					createTableTag += "<td class='today'>" + count + "</td>";
-// 				}else{
-// 					createTableTag += "<td>" + count + "</td>";
-// 				}
-// 			}
-// 		}
-// 		createTableTag += "</tr>";
-// 	}
-// 	return createTableTag;
-// }
+prev.addEventListener('click', () => {
+	currentSlide--;
+	slideImg();
+});

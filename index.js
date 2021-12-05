e147c679e24bf22fc052445eaa6a7c5493b4f076
misuzz <fs9dm3cc8ct1@gmail.com>
2021-12-05 22:49:22 +0900

    donewithcarousel

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ const prev = document.getElementById("prev");
 const ul = document.getElementById("medakaBabyUl");
 const slides = ul.children; //ulタグの子要素の情報を取る
 const slidewidth = slides[0].getBoundingClientRect().width;
+const dots = [];
 //getBoundingClientRect()でslides[0]の情報を取れる
 let currentSlide = 0;
 
@@ -36,22 +37,48 @@ function btnHidden() {
 	if (currentSlide == 0) {
 		prev.classList.add('btnHidden')
 	} else if (currentSlide == slides.length - 1) {
-		next.classList.add('btnHidden')
+		next.classList.add('btnHidden');
 	} else {
 		prev.classList.remove('btnHidden');
 		next.classList.remove('btnHidden');
 	}
 }
 
-//btnHiddenを実行しつつスライドさせる
+//ドットの色を移動させる
+function moveCurrentDots(){
+	dots.forEach(aDot => {
+		aDot.classList.remove('current');
+	});
+	dots[currentSlide].classList.add('current');
+}
+
+//btnHiddenを実行、ボタンを移動、画像をスライドさせる
 function slideImg() {
 	btnHidden();
+	moveCurrentDots();
 	ul.style.transform = `translateX(${-1 * slidewidth * currentSlide}px)`;
 }
 
+//画像の数だけ丸ボタンを作成する
+function createDots() {
+	for (let i = 0; i < slides.length; i++) {
+		const dot = document.createElement('button');
+		dot.addEventListener('click', () => {
+			currentSlide = i;
+			moveCurrentDots();
+			slideImg();
+		})
+		dots.push(dot);
+		document.querySelector('nav').appendChild(dot);
+	}
+
+	dots[0].classList.add('current');
+
+}
 
 
 btnHidden();
+createDots();
 
 next.addEventListener('click', () => {
 	currentSlide++;
